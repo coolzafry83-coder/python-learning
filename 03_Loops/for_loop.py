7d0a931c4fff5e87fa623082ae5d2b14a1e594f3
Muhammad Zafar <coolzafry83@gmail.com>
2026-07-20 20:57:45 +0500

    Completed for loop practice

---
+++ 03_Loops/for_loop.py
@@ -0,0 +1,17 @@
+# For loop practice
+
+for i in range(1, 11):
+    print(i)
+
+# Print even numbers
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+# Print fruits list
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+for fruit in fruits:
+    print(fruit)
